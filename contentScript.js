a854672d1bebfedae9e98ad052197dfd216cdafc
Nam Anh Nguyen <namanh11611@gmail.com>
2020-11-03 00:34:33 +0700

    Remove trial condition

--- contentScript.js
+++ contentScript.js
@@ -11,33 +11,26 @@ window.addEventListener("message", function (event) {
     }
 }, false);
 
-var dt = new Date();
-var m = dt.getMonth();
-var d = dt.getDate();
-if (m == 9 || (m == 10 && d < 5)) {
-    var isEnableIron;
-    var isIronActive;
-    var timeOutIron = 0;
-    chrome.storage.sync.get(['enable_iron', 'iron_active', 'timeout_iron'], function (result) {
-        isEnableIron = result.enable_iron == "yes";
-        isIronActive = result.iron_active == "yes";
-        timeOutIron = result.timeout_iron;
-        console.log("Get Iron Man: " + isEnableIron + ", time = " + timeOutIron);
-    });
+var isEnableIron;
+var isIronActive;
+var timeOutIron = 0;
+chrome.storage.sync.get(['enable_iron', 'iron_active', 'timeout_iron'], function (result) {
+    isEnableIron = result.enable_iron == "yes";
+    isIronActive = result.iron_active == "yes";
+    timeOutIron = result.timeout_iron;
+});
 
-    setTimeout(function () {
-        if (isIronActive) {
+setTimeout(function () {
+    if (isIronActive) {
+        goToSleep();
+    } else if (isEnableIron) {
+        var timeCountDown = timeOutIron * 60000;
+        setTimeout(function () {
             goToSleep();
-        } else if (isEnableIron) {
-            var timeCountDown = timeOutIron * 60000;
-            console.log("Iron Man is comming " + timeCountDown);
-            setTimeout(function () {
-                goToSleep();
-                chrome.storage.sync.set({ iron_active: 'yes' });
-            }, timeCountDown);
-        }
-    }, 5000);
-}
+            chrome.storage.sync.set({ iron_active: 'yes' });
+        }, timeCountDown);
+    }
+}, 5000);
 
 function goToSleep() {
     $("ytd-app").hide();
