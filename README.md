c25cd229a3ab623a266e7a8221dae6d2913a9160
Nam Anh Nguyen <namanh11611@gmail.com>
2020-11-03 00:30:14 +0700

    Add text after hide Youtube and add file jquery

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Iron-man Extension
 Iron-man Extension for Youtube on Chrome browser, bypass Youtube popup confirm.
 
-Feature:
-1. Bypass Youtube popup confirm
+## Feature
+- Bypass Youtube popup confirm
+- Set timeout to hide Youtube for kid
--- contentScript.js
+++ contentScript.js
@@ -1,48 +1,46 @@
 var port = chrome.runtime.connect();
 
-window.addEventListener("message", function(event) {
-  // We only accept messages from ourselves
-  if (event.source != window)
-    return;
+window.addEventListener("message", function (event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
 
-  if (event.data.type && (event.data.type == "FROM_PAGE")) {
-    console.log("Content script received: " + event.data.text);
-    port.postMessage(event.data.text);
-  }
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        console.log("Content script received: " + event.data.text);
+        port.postMessage(event.data.text);
+    }
 }, false);
 
 var dt = new Date();
 var m = dt.getMonth();
 var d = dt.getDate();
 if (m == 9 || (m == 10 && d < 5)) {
-  var isEnableIron;
-  var isIronActive;
-  var timeOutIron = 0;
-  chrome.storage.sync.get(['enable_iron', 'iron_active', 'timeout_iron'], function(result) {
-    isEnableIron = result.enable_iron == "yes";
-    isIronActive = result.iron_active == "yes";
-    timeOutIron = result.timeout_iron;
-    console.log("Get Iron Man: " + isEnableIron + ", time = " + timeOutIron);
-  });
+    var isEnableIron;
+    var isIronActive;
+    var timeOutIron = 0;
+    chrome.storage.sync.get(['enable_iron', 'iron_active', 'timeout_iron'], function (result) {
+        isEnableIron = result.enable_iron == "yes";
+        isIronActive = result.iron_active == "yes";
+        timeOutIron = result.timeout_iron;
+        console.log("Get Iron Man: " + isEnableIron + ", time = " + timeOutIron);
+    });
 
-  setTimeout(function() {
-    if (isIronActive) {
-      goToSleep();
-    } else if (isEnableIron) {
-      var timeCountDown = timeOutIron * 60000;
-      console.log("Iron Man is comming " + timeCountDown);
-      setTimeout(function() {
-        goToSleep();
-        chrome.storage.sync.set({iron_active: 'yes'});
-      }, timeCountDown);
-    }
-  }, 5000);
+    setTimeout(function () {
+        if (isIronActive) {
+            goToSleep();
+        } else if (isEnableIron) {
+            var timeCountDown = timeOutIron * 60000;
+            console.log("Iron Man is comming " + timeCountDown);
+            setTimeout(function () {
+                goToSleep();
+                chrome.storage.sync.set({ iron_active: 'yes' });
+            }, timeCountDown);
+        }
+    }, 5000);
 }
 
 function goToSleep() {
-  document.body.style.display = "none";
-  var videos = document.getElementsByTagName('VIDEO');
-  if (videos && videos.length > 0) {
-    videos[0].pause();
-  }
+    $("ytd-app").hide();
+    $("video").get(0).pause();
+    $("body").append("<p style='text-align: center; margin: 200px; font-size: 40px;'>Đã hết thời gian xem Youtube. Vui lòng nghỉ ngơi và dành thời gian ra ngoài vui chơi.</p>");
 }
